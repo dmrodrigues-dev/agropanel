afb80d3e06375e0fd2fc1ca37e844a59d6ffe254
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-06-24 21:51:45 -0300

    fix(api): enable CORS to allow cross-origin requests

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from blueprints import produtos, compras, vendas
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(produtos.produtos_bp)
 app.register_blueprint(compras.compras_bp)
