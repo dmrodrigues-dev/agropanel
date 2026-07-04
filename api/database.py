f9edcc866b1d4f58f01ed7b6b896b1b5ad7de059
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-07-04 17:22:09 -0300

    fix(database): fail fast and log critical error on pool initialization

--- api/database.py
+++ api/database.py
@@ -1,15 +1,20 @@
-import mysql.connector
 from dotenv import load_dotenv
 import os
+import loggers
 
 from mysql.connector.pooling import MySQLConnectionPool
 
 load_dotenv()
 
-pool = MySQLConnectionPool(pool_size=5,
+try:
+    pool = MySQLConnectionPool(pool_size=5,
                            pool_name= 'agropanel',
                            host=os.getenv('DB_HOST'),
                            user=os.getenv('DB_USER'),
                            password=os.getenv('DB_PASSWORD'),
                            database=os.getenv('DB_DATABASE')
-)
+    )
+
+except Exception as e:
+    loggers.app_logger.critical(f'{e}')
+    raise
