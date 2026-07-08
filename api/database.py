81c6f8a3ccec205f2b72de8542b38d07d64810be
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-07-08 02:59:45 -0300

    feat(database): configure ssl connection for managed mysql

--- api/database.py
+++ api/database.py
@@ -6,13 +6,18 @@ from mysql.connector.pooling import MySQLConnectionPool
 
 load_dotenv()
 
+raiz = os.path.dirname(os.path.dirname(__file__))
+
 try:
     pool = MySQLConnectionPool(pool_size=5,
                            pool_name= 'agropanel',
                            host=os.getenv('DB_HOST'),
+                           port=os.getenv('DB_PORT'),
                            user=os.getenv('DB_USER'),
                            password=os.getenv('DB_PASSWORD'),
-                           database=os.getenv('DB_DATABASE')
+                           database=os.getenv('DB_DATABASE'),
+                           ssl_ca=os.path.join(raiz, 'certs', os.getenv('CA_CERTIFICATE')),
+                           ssl_verify_cert=True
     )
 
 except Exception as e:
