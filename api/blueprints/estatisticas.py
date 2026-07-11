d87f76f8b78c0a80d2e10b6464d4be31a04eb1ad
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-07-11 02:02:41 -0300

    refactor(api): migrate estatisticas routes to sqlalchemy

--- api/blueprints/estatisticas.py
+++ api/blueprints/estatisticas.py
@@ -1,6 +1,7 @@
 import database
 import utils
 from flask import Blueprint, jsonify, request
+from sqlalchemy import text
 
 estatisticas_bp = Blueprint('estatisticas', __name__)
 
@@ -8,49 +9,41 @@ estatisticas_bp = Blueprint('estatisticas', __name__)
 # Rota para retornar as estatísticas de um mês
 @estatisticas_bp.route('/api/estatisticas', methods=['GET'])
 def estatisticas_gerais():
-    # Conecta na pool e cria o cursor
-    db = database.pool.get_connection()
-    cursor = db.cursor()
 
     # Recebe argumentos da rota
     mes = request.args.get('mes')
     ano = request.args.get('ano')
 
-    try:
+    # Com uma conexão da engine feita, execute o bloco
+    with database.engine.connect() as conn:
+
         # Valida mes e ano e recebe um filtro para a query
         filtro = utils.month_and_year_validation(mes, ano)
 
         # Busca a soma da multiplicação de quantidade e do valor de cada compra
-        cursor.execute("select coalesce(sum(preco*qtd), 0) from compras where date_format(dia, '%m-%Y') = %s ", (filtro,))
-        despesa = cursor.fetchone()[0]
+        query = conn.execute(text("select coalesce(sum(preco*qtd), 0) from compras where to_char(dia, 'MM-YYYY') = :filtro "), {'filtro': filtro})
+        despesa = query.fetchone()[0]
 
         # Busca a soma da multiplicação de quantidade e do valor de cada venda
-        cursor.execute("select coalesce(sum(preco*qtd), 0) from vendas where date_format(dia, '%m-%Y') = %s ", (filtro,))
-        receita = cursor.fetchone()[0]
+        query = conn.execute(text("select coalesce(sum(preco*qtd), 0) from vendas where to_char(dia, 'MM-YYYY') = :filtro "), {'filtro': filtro})
+        receita = query.fetchone()[0]
 
         # Calcula lucro
         lucro = receita - despesa
 
         # Busca quais 10 produtos mais renderam no mes
-        cursor.execute("select p.nome, sum(v.qtd*v.preco) as valor from vendas as v " \
+        query = conn.execute(text("select p.nome as nome, sum(v.qtd*v.preco) as valor from vendas as v " \
         "join produtos as p on v.produto_id = p.id " \
-        "where date_format(v.dia, '%m-%Y') = %s " \
-        "group by v.produto_id order by valor desc limit 10;", (filtro,))
+        "where to_char(v.dia, 'MM-YYYY') = :filtro " \
+        "group by v.produto_id, p.nome order by valor desc limit 10;"), {'filtro': filtro})
         # Lista de tuplas no formato ('nome', valor)
-        raw_vendaveis = cursor.fetchall()
-        vendaveis = []
-
-        for item in raw_vendaveis:
-            vendaveis.append({'nome': item[0], 'valor':item[1]})
+        raw_vendaveis = query.fetchall()
+        vendaveis = [utils.type_casted_dict(dict(row._mapping)) for row in raw_vendaveis]
 
         # Retorno
-        return jsonify({
+        return jsonify(utils.type_casted_dict({
             'receita': receita,
             'despesa': despesa,
             'lucro': lucro,
             'vendaveis': vendaveis
-        })
-
-    finally:
-        cursor.close()
-        db.close()
+        }))
