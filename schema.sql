acc846c825403b3a92f03f12688a67d90f9e8547
Davi Matos Rodrigues <davimatosrodrigues02@gmail.com>
2026-08-17 17:51:32 -0300

    feat: add estoque tracking via triggers on compras and vendas

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@
 create table produtos (
   id bigint generated always as identity primary key,
   nome varchar(80) not null,
-  preco_de_venda numeric(10,2) not null
+  preco_de_venda numeric(10,2) not null,
+  estoque int not null default 0
 );
 
 CREATE TABLE compras (
@@ -28,3 +29,61 @@ CREATE TABLE vendas (
     FOREIGN KEY (produto_id)
     REFERENCES produtos(id)
 );
+
+create or replace function put_estoque()
+returns trigger as $$
+begin
+  if old.produto_id = new.produto_id then
+    update produtos
+    set estoque = estoque - (old.qtd * TG_ARGV[0]::integer) + (new.qtd * TG_ARGV[0]::integer)
+    where id = old.produto_id;
+  else
+    update produtos
+    set estoque = estoque - (old.qtd * TG_ARGV[0]::integer)
+    where id = old.produto_id;
+    update produtos
+    set estoque = estoque + (new.qtd * TG_ARGV[0]::integer)
+    where id = new.produto_id;
+  end if;
+  return new;
+end;
+$$ language plpgsql;
+
+create or replace function insert_or_delete_estoque()
+returns trigger as $$
+begin
+  if (TG_OP = 'INSERT') then
+    update produtos
+    set estoque = estoque + new.qtd * TG_ARGV[0]::integer
+    where id = new.produto_id;
+    return new;
+  elsif TG_OP = 'DELETE' then
+    update produtos
+    set estoque = estoque - old.qtd * TG_ARGV[0]::integer
+    where id = old.produto_id;
+    return old;
+  end if;
+end;
+$$ language plpgsql;
+
+create trigger add_del_compra_gatilho
+after insert or delete on compras
+for each row
+execute function insert_or_delete_estoque('1');
+
+create trigger add_del_vendas_gatilho
+after insert or delete on vendas
+for each row
+execute function insert_or_delete_estoque('-1');
+
+create trigger put_compra_gatilho
+after update on compras
+for each row
+when (old.qtd is distinct from new.qtd or old.produto_id is distinct from new.produto_id)
+execute function put_estoque('1');
+
+create trigger put_venda_gatilho
+after update on vendas
+for each row
+when (old.qtd is distinct from new.qtd or old.produto_id is distinct from new.produto_id)
+execute function put_estoque('-1');
